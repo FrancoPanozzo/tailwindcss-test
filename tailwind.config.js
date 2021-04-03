3db9109c9c0b847e3afef3625c8dfafb9065eb2f
Franco <francopanozzo@hotmail.com.ar>
2021-04-03 20:47:41 -0300

    Added google fonts links and implemented new classes in the tailwind config for them

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   // purge: { enabled: true, content: ['./dist/**/*.html'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        'sans-title': ['Roboto Condensed', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
